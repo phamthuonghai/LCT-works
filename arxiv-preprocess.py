f53bd493cbc781afa5696ab5703e1a283cac24d8
Thuong-Hai Pham <phamthuonghai@gmail.com>
2017-01-02 23:04:05 +0100

    Data stats & preprocess.py

---
+++ arxiv-preprocess.py
@@ -0,0 +1,43 @@
+from tqdm import tqdm
+import json
+import re
+import bz2
+import datetime
+import pandas as pd
+
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+
+# year, month, day, hour, minute
+data_time_start = datetime.datetime(2016, 7, 01, 0, 0)
+data_time_end = datetime.datetime(2016, 7, 31, 23, 59)
+
+def time_range(start_time, end_time):
+    while start_time <= end_time:
+        start_time += datetime.timedelta(minutes=1)
+        yield start_time
+
+# Process data from raw files
+
+tweets_json = []
+
+for cur_time in tqdm(time_range(data_time_start, data_time_end)):
+    try:
+        with bz2.BZ2File('/media/phamthuonghai/DATA/twitter-data/%s.json.bz2' 
+                         % cur_time.strftime('%Y/%m/%d/%H/%M'), 'r') as f:
+            data_lines = f.readlines()
+            tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
+            punc = set('!$%^&*()_-+=\|{}[]:;"\'<>,.?/')
+
+            for data_line in data_lines:
+                tmp = json.loads(data_line)
+                if 'text' in tmp and ('lang' in tmp and tmp['lang'] == 'en'):
+                    tmp['text'] = [w for w in tknzr.tokenize(tmp['text'].lower()) if re.match('^#?\w+$', w)]
+                    tweets_json.append([tmp['text'], tmp['lang'], tmp['user']['id']])
+    except:
+        pass
+
+tweets = pd.DataFrame(tweets_json, columns=['text', 'lang', 'user'])
+
+tweets.to_pickle('./data_%s_%s.pkl' % (data_time_start.strftime('%Y-%m-%d-%H-%M'),
+                                              data_time_end.strftime('%Y-%m-%d-%H-%M')))
