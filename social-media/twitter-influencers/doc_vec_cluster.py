f022b0803dc044c8f54f7bcc9c9054bd53148699
Thuong-Hai Pham <phamthuonghai@gmail.com>
2017-02-01 01:33:10 +0100

    [social-media] add doc_vec_cluster.py

---
+++ social-media/twitter-influencers/doc_vec_cluster.py
@@ -0,0 +1,82 @@
+'''
+to train doc2vec:
+    python doc_vec_cluster.py train_doc2vec -t ./data/data_tweets_selected.pkl -d ./data/doc2vec_model.model
+'''
+# encoding=utf8  
+import sys  
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+import argparse
+import pickle
+import pandas as pd
+from timeit import default_timer as timer
+from gensim import models
+from sklearn.cluster import KMeans
+
+
+def train_doc2vec(tweets_file, doc2vec_file, author_topic, workers, iterations, feature_size):
+    print('Loading tweets from ' + tweets_file)
+    tweets = pd.read_pickle(tweets_file)
+
+    if author_topic:
+        tweets = tweets.groupby('user').agg({'text': 'sum'})
+
+    print('Writing %d tweets to tmp file' % len(tweets.index))
+
+    tmp_file = doc2vec_file + '.docs'
+
+    with open(tmp_file, 'w') as f_tmp:
+        for item in tweets.text:
+            f_tmp.write(' '.join(item) + '\n')
+
+    sentences = models.doc2vec.TaggedLineDocument(tmp_file)
+    model = models.doc2vec.Doc2Vec(sentences, size=feature_size, window=10, min_count=5,
+        iter=iterations, workers=workers)
+
+    print('Writing doc2vec model to ' + doc2vec_file)
+    model.save(doc2vec_file)
+
+
+def train_kmeans(doc2vec_file, cluster_file, topics_num):
+    print("Loading Doc2Vec model")
+    md_doc2vec = models.Doc2Vec.load(doc2vec_file)
+    md_kmean = KMeans(n_clusters=topics_num, n_jobs=-1)
+
+    print("Training K-means with %d clusters" % topics_num)
+    kmean = md_kmean.fit(md_doc2vec.docvecs)
+
+    print("Writing K-means result to " + cluster_file)
+    with open(cluster_file, 'wb') as pickle_file:
+        pickle.dump(kmean, pickle_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("task", help="task to do: train_doc2vec, train_kmeans")
+    parser.add_argument("-t", "--tweets", help="Tweets file to parse")
+    parser.add_argument("-c", "--cluster", help="Cluster model file")
+    parser.add_argument("-d", "--doc2vec", help="Doc2vec model file")
+    parser.add_argument("-m", "--model", help="Model file")
+    parser.add_argument("-k", "--topics_num", type=int, default=20,
+                        help="Number of topics")
+    parser.add_argument("-i", "--iterations", type=int, default=20,
+                        help="Number of iterations")
+    parser.add_argument("-w", "--workers", type=int, default=4,
+                        help="Number of workers")
+    parser.add_argument("-f", "--feature_size", type=int, default=100,
+                        help="Number of workers")
+    parser.add_argument("-a", "--author-topic", help="Author topic model flag",
+                        action="store_true")
+    args = parser.parse_args()
+
+    if args.task == 'train_doc2vec':
+        start = timer()
+        train_doc2vec(args.tweets, args.doc2vec, args.author_topic,
+                        args.workers, args.iterations, args.feature_size)
+        print('Time elasped ', timer() - start)
+    elif args.task == 'train_kmeans':
+        start = timer()
+        train_kmeans(args.doc2vec, args.cluster, args.topics_num)
+        print('Time elasped ', timer() - start)
--- social-media/twitter-influencers/lda.py
+++ social-media/twitter-influencers/lda.py
@@ -1,13 +1,15 @@
 '''
 to prepare corpus:
-    python lda.py prepare_corpus --tweets ./data/data_tweets_selected.pkl 
-                --corpus ./data/lda_sep_corpus.mm --dictionary ./data/lda_sep_dict.dict
+    python lda.py prepare_corpus -t ./data/data_tweets_selected.pkl 
+                -c ./data/lda_sep_corpus.mm -d ./data/lda_sep_dict.dict
 to train:
-    python lda.py train --corpus ./data/lda_sep_corpus.mm --dictionary ./data/lda_sep_dict.dict
-                --model ./data/lda_sep_model.model
-to visualise:
-    python lda.py visualise --corpus ./data/lda_sep_corpus.mm --dictionary ./data/lda_sep_dict.dict
-                --model ./data/lda_sep_model.model
+    python lda.py train -c ./data/lda_sep_corpus.mm -d ./data/lda_sep_dict.dict
+                -m ./data/lda_sep_model.model -i 100
+to visualise: # better to use Jupyter notebook visualise.ipynb
+    python lda.py visualise -c ./data/lda_sep_corpus.mm -d ./data/lda_sep_dict.dict
+                -m ./data/lda_sep_model.model
+to show topics:
+    python lda.py show_topics -m ./data/lda_sep_model.model
 for more parameters:
     python lda.py -h
 '''
@@ -31,14 +33,16 @@ from nltk.tokenize import TweetTokenizer
 from nltk.corpus import stopwords
 
 def prepare_corpus(tweets_file, corpus_file, dictionary_file, author_topic):
-    print('Loading tweets from ' + tweets_file)
     stop_words = set(stopwords.words('english'))
     stop_words.add(u'rt')
 
+    print('Loading tweets from ' + tweets_file)
     tweets = pd.read_pickle(tweets_file)
 
     if author_topic:
         tweets = tweets.groupby('user').agg({'text': 'sum'})
+
+    print('%d tweets loaded' % len(tweets.index))
 
     dictionary = Dictionary(tweets['text'])
     stopword_ids = map(dictionary.token2id.get, stop_words)
@@ -57,20 +61,22 @@ def prepare_corpus(tweets_file, corpus_file, dictionary_file, author_topic):
     
     dictionary.save(dictionary_file)
 
-def train(corpus_file, dictionary_file, model_file, no_topic, no_pass, no_worker):
+def train(corpus_file, dictionary_file, model_file, no_topic, no_iteration, no_worker):
     print('Loading corpus from ' + corpus_file)
     corpus = MmCorpus(corpus_file)
     print('Loading dictionary from ' + dictionary_file)
     dictionary = Dictionary.load(dictionary_file)
 
-    print('Training model %d topics in %d passes with %d workers' % (no_topic, no_pass, no_worker))
-    lda = models.ldamulticore.LdaMulticore(corpus=corpus, id2word=dictionary,
-                                            num_topics=no_topic, passes=no_pass, workers=no_worker)
+    print('Training model %d topics in %d interations with %d workers' % (no_topic, no_iteration, no_worker))
+    lda = models.ldamulticore.LdaMulticore(corpus=corpus, id2word=dictionary, num_topics=no_topic,
+                                            iterations=no_iteration, workers=no_worker)
 
     print('Writing model to ' + model_file)
     lda.save(model_file)
 
 def visualise(model_file, corpus_file, dictionary_file):
+    # use Notebook version if not working
+
     print('Loading corpus from ' + corpus_file)
     corpus = MmCorpus(corpus_file)
     print('Loading dictionary from ' + dictionary_file)
@@ -78,20 +84,27 @@ def visualise(model_file, corpus_file, dictionary_file):
     print('Loading model from ' + model_file)
     model = models.ldamulticore.LdaMulticore.load(model_file)
 
-    vis_data = gensimvis.prepare(lda, corpus, dictionary)
+    vis_data = gensimvis.prepare(model, corpus, dictionary)
     pyLDAvis.display(vis_data)
+    print('Please use Jupyter notebook visualise.ipynb if not working')
+
+def show_topics(model_file):
+    print('Loading model from ' + model_file)
+    model = models.ldamulticore.LdaMulticore.load(model_file)
+    print(model.show_topics(num_topics=-1, num_words=15, formatted=False))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("task", help="task to do: prepare_corpus, train, visualise")
+    parser.add_argument("task", help="task to do: prepare_corpus, train, visualise, show_topics")
     parser.add_argument("-t", "--tweets", help="Tweets file to parse")
     parser.add_argument("-c", "--corpus", help="Corpus file")
     parser.add_argument("-d", "--dictionary", help="Dictionary file")
     parser.add_argument("-m", "--model", help="Model file")
     parser.add_argument("-k", "--topics_num", type=int, default=20,
                         help="Number of topics")
-    parser.add_argument("-p", "--passes", type=int, default=20,
-                        help="Number of passes")
+    parser.add_argument("-i", "--iterations", type=int, default=100,
+                        help="Number of iterations")
     parser.add_argument("-w", "--workers", type=int, default=3,
                         help="Number of workers")
     parser.add_argument("-a", "--author-topic", help="Author topic model flag",
@@ -102,7 +115,9 @@ if __name__ == '__main__':
         prepare_corpus(args.tweets, args.corpus, args.dictionary, args.author_topic)
     elif args.task == 'train':
         start = timer()
-        train(args.corpus, args.dictionary, args.model, args.topics_num, args.passes, args.workers)
-        print('Time elasped ', time() - start)
+        train(args.corpus, args.dictionary, args.model, args.topics_num, args.iterations, args.workers)
+        print('Time elasped ', timer() - start)
     elif args.task == 'visualise':
         visualise(args.model, args.corpus, args.dictionary)
+    elif args.task == 'show_topics':
+        show_topics(args.model)
